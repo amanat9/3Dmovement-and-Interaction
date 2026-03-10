f7e282dac433c2f03a12a1247c52f7ebcb9ed4d0
Samiha_Sultana <Samihasultana100@gmail.com>
2026-03-09 17:44:22 -0700

    reload Done

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -5,8 +5,19 @@ using TMPro;
 
 public class GunShoot : MonoBehaviour
 {
-    public GameObject muzzleEffect;
+
+    //Reload
+    public float reloadTime;
+    public int magazineSize;
+        public int bulletleft;
+    public bool isReloading;
+
+    //animation 
     private Animator m_Animator;
+    //muzzle effect 
+    public GameObject muzzleEffect;
+ 
+
     public SoundPlayer soundPlayer;
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
@@ -14,11 +25,14 @@ public class GunShoot : MonoBehaviour
     public TextMeshProUGUI enemyText;
     public int enemyDestroyed = 0;
     public float firingDelay = 0f;
-
+    public TextMeshProUGUI AmmoText;
 
     private void Start()
     {
+        //animation 
         m_Animator = GetComponent<Animator>();
+        //Reload
+        bulletleft = magazineSize;
     }
 
 
@@ -27,17 +41,51 @@ public class GunShoot : MonoBehaviour
     {
         if (Input.GetMouseButton(0) && firingDelay <=0 )
         {
-            m_Animator.SetTrigger("Recoil");
-            muzzleEffect.GetComponent<ParticleSystem>().Play();
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.SetActive(true);
-            soundPlayer.PlayGunFire();
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-            firingDelay = 0.5f;
+            if (bulletleft > 0)
+            {
+                fireGun();
+            }
         }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        { 
+        
+            Reload();
+        
+        }
+
+
 
         firingDelay = firingDelay - Time.deltaTime;
 
         enemyText.text = "Enemy Destroyed: " + enemyDestroyed;
+        AmmoText.text = "Ammo: " +bulletleft +"/"+ magazineSize;
+    }
+
+
+    private void Reload()
+    { 
+        isReloading = true;
+        Invoke("ReloadCompleted", reloadTime);
+
+    }
+    public void ReloadCompleted()
+    {
+        bulletleft = magazineSize;
+        isReloading=false;
+    }
+
+    void fireGun()
+    {
+        bulletleft--;
+        //animation 
+        m_Animator.SetTrigger("Recoil");
+        //muzzle effect 
+        muzzleEffect.GetComponent<ParticleSystem>().Play();
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bullet.SetActive(true);
+        soundPlayer.PlayGunFire();
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        firingDelay = 0.5f;
     }
 }
