6d0579dd0f95f27e74e6b52acc3fb9831f6b4df5
Samiha_Sultana <Samihasultana100@gmail.com>
2026-04-15 19:23:29 -0700

    added Green boxes

---
+++ Assets/practiceL11.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class practiceL11 : MonoBehaviour
+{
+
+    string s = "racecar"
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (palindromeChecker())
+        {
+            Debug.Log("True");
+        } //else
+
+    }
+
+    bool palindromeChecker()
+
+    {
+
+        return false;
+    }
+
+
+
+  
+}
